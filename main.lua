48ecb04703935f0ede94cdcec1d7ed1a94e9a090
Inoregano <unauthorizedfruit@gmail.com>
2025-10-01 14:40:37 -0400

    entities

--- main.lua
+++ main.lua
@@ -1,3 +1,50 @@
+love.graphics.setDefaultFilter("nearest")
+math.randomseed(os.time())
+
+local gl = {
+	cellSize = 16,
+	windowWidth = 240,
+	windowHeight = 160,
+	windowScale = 2,
+}
+function gl.clamp(val, min, max) if val < min then return min elseif val > max then return max end return val end
+love.window.setMode(gl.windowWidth * gl.windowScale, gl.windowHeight * gl.windowScale)
+
+function newSheet(img, cellSize)
+	local sheet = {
+		img = love.graphics.newImage(img),
+		quads = {},
+	}
+	sheet.width = sheet.img:getWidth() / cellSize
+	sheet.height = sheet.img:getHeight() / cellSize
+	sheet.batch = love.graphics.newSpriteBatch(sheet.img)
+	for y = 1, sheet.height do
+		sheet.quads[y] = {}
+		for x = 1, sheet.width do
+			sheet.quads[y][x] = love.graphics.newQuad(
+				(x - 1) * cellSize,
+				(y - 1) * cellSize,
+				cellSize, cellSize,
+				sheet.img
+			)
+		end
+	end
+
+	return sheet
+end
+
+local a = {
+	tiles = newSheet("assets/tiles.png", gl.cellSize),
+}
+
+function newEntity(name)
+	local entity = {
+		occupies = {2, 2},
+		check = "hello and welcome to the test entity.\ni hope you have a great day"
+	}
+	entity.name = name
+	return entity
+end
 function newRoom(width, height)
 	local room = {
 		width = width,
@@ -5,5 +52,131 @@ function newRoom(width, height)
 		tiles = {},
 		entities = {},
 	}
+	for y = 1, room.height do
+		room.tiles[y] = {}
+		for x = 1, room.width do
+			room.tiles[y][x] = {
+				value = 2,
+				type = 1,
+				canWalk = true,
+				entity = {
+					key = "",
+					isPointer = false,
+					pointer = {},
+				},
+			}
+		end
+	end
+	
+	function room:addEntity(name, x, y)
+		local key 
+		--this is surely the cleanest way of generating a random string
+		while true do
+			key =
+				string.char(math.random(65, 65 + 25)):lower()..
+				string.char(math.random(65, 65 + 25)):lower()..
+				string.char(math.random(65, 65 + 25)):lower()..
+				string.char(math.random(65, 65 + 25)):lower()..
+				string.char(math.random(65, 65 + 25)):lower()..
+				string.char(math.random(65, 65 + 25)):lower()
+			if self.entities[key] == nil then
+				break
+			end
+		end
+		
+		self.entities[key] = newEntity(name)
+		self.entities[key].x, self.entities[key].y = x, y
 
+		self.tiles[y][x].entity.key = key
+		for ty = 1, self.entities[key].occupies[2] do
+			for tx = 1, self.entities[key].occupies[1] do
+				if not (ty == 1 and tx == 1) then
+					self.tiles[ty][tx].entity.isPointer = true
+					self.tiles[ty][tx].entity.pointer = {x, y}
+				end
+			end
+		end
+	end
+	
+	function room:getEntity(x, y, type, reps)
+		reps = reps or 0
+		if self.tiles[y][x].entity.isPointer then
+			if reps > 0 then 
+				print(self.tiles[y][x].entity.pointer[1], self.tiles[y][x].entity.pointer[2])
+				error("infinitely repeating getEntity at "..x.." "..y) 
+			end
+			return self:getEntity(
+				self.tiles[y][x].entity.pointer[1],
+				self.tiles[y][x].entity.pointer[2],
+				type, 1
+			)
+		end
+		if self.tiles[y][x].entity.key == "" then return nil end
+		if type == "key" then
+			return self.tiles[y][x].entity.key
+		end
+
+		if self.entities[self.tiles[y][x].entity.key] == nil then
+			error("pointer to nil entity")
+		end
+		return self.entities[self.tiles[y][x].entity.key]
+	end
+
+	return room
 end
+
+local room = newRoom(8, 8)
+room.tiles[2][2].value, room.tiles[2][2].canWalk = 1, false
+room:addEntity("test", 1, 1)
+
+local mouse = {
+	x = 1,
+	absx = 1,
+	y = 1,
+	absy = 1,
+}
+
+function love.update(dt)
+	mouse.absx = math.floor(love.mouse.getX() / gl.windowScale)
+	mouse.absy = math.floor(love.mouse.getY() / gl.windowScale)
+	
+	mouse.x = math.floor(mouse.absx / gl.cellSize) + 1
+	mouse.y = math.floor(mouse.absy / gl.cellSize) + 1
+end
+
+local fullCanvas = love.graphics.newCanvas(gl.windowWidth, gl.windowHeight)
+love.graphics.setLineWidth(1)
+function love.draw()
+	love.graphics.setCanvas(fullCanvas)
+	love.graphics.clear()
+	
+	a.tiles.batch:clear()
+	for y = 1, room.height do
+		for x = 1, room.width do
+			a.tiles.batch:add(a.tiles.quads[room.tiles[y][x].value][room.tiles[y][x].type],
+				(x - 1) * gl.cellSize, (y - 1) * gl.cellSize
+			)
+		end
+	end
+	love.graphics.draw(a.tiles.batch)
+	
+	love.graphics.setColor(0, 1, 1)
+
+	love.graphics.rectangle("line",
+		(mouse.x - 1) * gl.cellSize, (mouse.y - 1) * gl.cellSize,
+		gl.cellSize, gl.cellSize
+	)
+	love.graphics.setColor(0, 1, 0)
+	love.graphics.setColor(1, 1, 1)
+	
+	if room:getEntity(mouse.x, mouse.y) then
+		love.graphics.print(
+			room:getEntity(mouse.x, mouse.y).check,
+			0, room.height * gl.cellSize
+		)
+	end
+	
+	love.graphics.setCanvas()
+	love.graphics.draw(fullCanvas, 0, 0, 0, gl.windowScale, gl.windowScale)
+end
+
